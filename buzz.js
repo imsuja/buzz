bba52c5c45622606451a2b302510b42f2680a314
imsuja <95766583+imsuja@users.noreply.github.com>
2024-02-18 13:54:55 +0530

    Update buzz.js

--- buzz.js
+++ buzz.js
@@ -48,22 +48,29 @@
     if(a!=null) a.src = 'https://res.cloudinary.com/trailhead/image/upload/public-trailhead/assets/images/ranks/triple-star-ranger.png'
 
     a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.progress > img:nth-child(3)");
-if(a != null) a.src = "https://res.cloudinary.com/trailhead/image/upload/public-trailhead/assets/images/ranks/triple-star-ranger.png"
+    if(a != null) a.src = "https://res.cloudinary.com/trailhead/image/upload/public-trailhead/assets/images/ranks/triple-star-ranger.png"
 
-a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.progress > img:nth-child(1)")
-if(a != null) a.src = "https://res.cloudinary.com/trailhead/image/upload/public-trailhead/assets/images/ranks/double-star-ranger.png"
+    a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.progress > img:nth-child(1)")
+    if(a != null) a.src = "https://res.cloudinary.com/trailhead/image/upload/public-trailhead/assets/images/ranks/double-star-ranger.png"
 
-a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.summary > lwc-thtoday-about-me-rank-requirements")?.shadowRoot.querySelector("lwc-tds-sanitized-html > span")
-if (a != null) a.innerHTML = 'Earn <b>79</b> more badges and <b>49,900</b> more points to reach <a href="https://trailhead.salesforce.com/trailblazer-ranks" target="_blank" rel="noopener">Triple Star Ranger rank.</a>.'
+    a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.summary > lwc-thtoday-about-me-rank-requirements")?.shadowRoot.querySelector("lwc-tds-sanitized-html > span")
+    if (a != null) a.innerHTML = 'Earn <b>79</b> more badges and <b>49,900</b> more points to reach <a href="https://trailhead.salesforce.com/trailblazer-ranks" target="_blank" rel="noopener">Triple Star Ranger rank.</a>.'
 
-a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.summary > div")
-if(a!=null) a.innerHTML = "You have 100,100 points"
+    a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.summary > div")
+    if(a!=null) a.innerHTML = "You have 100,100 points"
 
-a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.progress > lwc-tds-progress-bar")?.shadowRoot.querySelector("div > div")
-if (a != null) a.style.width = '1%';
+    a = document.querySelector("#main > thtoday-page")?.shadowRoot.querySelector("#about-me")?.shadowRoot.querySelector("div.progress > lwc-tds-progress-bar")?.shadowRoot.querySelector("div > div")
+    if (a != null) a.style.width = '1%';
 
-a = document.querySelector("#global-nav")?.shadowRoot.querySelector("header > div.c360-nav__header > div.c360-nav__wrapper > nav.utility-nav.show > ul > li.utility-icons-items.login > hgf-c360login")?.shadowRoot.querySelector("#login-auth-content > div > h4.section-title.trailhead-badge-info")
-if ( a != null) a.innerHTML = 'Double Star Ranger · 221 badges · 100100 points'
+    a = document.querySelector("#global-nav")?.shadowRoot.querySelector("header > div.c360-nav__header > div.c360-nav__wrapper > nav.utility-nav.show > ul > li.utility-icons-items.login > hgf-c360login")?.shadowRoot.querySelector("#login-auth-content > div > h4.section-title.trailhead-badge-info")
+    if ( a != null) a.innerHTML = 'Double Star Ranger · 221 badges · 100100 points'
+
+    a = document.querySelector("#profile-sections-container > div:nth-child(3) > tbme-rank")?.shadowRoot.querySelector("lwc-tds-theme-provider > lwc-tbui-card > div:nth-child(3) > div > lwc-tds-dangerous-html > span")
+    if (a != null) a.innerHTML = 'Earn <b>79</b> more badges and  <b>49,900</b> more points to reach <a href="https://trailhead.salesforce.com/en/trailblazer-ranks" class="rank-name-container" target="_blank"><span class="capitalize">Expeditioner</span> rank</a>.'
+
+    a = document.querySelector("#profile-sections-container > div:nth-child(3) > tbme-rank").shadowRoot.querySelector("lwc-tds-theme-provider > lwc-tbui-card > div:nth-child(3) > div > lwc-tbui-progress-avatar").shadowRoot.querySelector("div > div.progress > svg > path");
+    if (a != null) a.style.display = 'none';
+
 }, 1000);
 
 })();
